d6767f7850686bdd3d80eac7bb92e445d6e3869d
swayaminsync <hawkempire007@gmail.com>
2025-12-06 14:39:16 +0530

    adding sleef building script

---
+++ build.sh
@@ -0,0 +1,32 @@
+rm -rf build/
+mkdir build
+
+# Configure and build with /usr/local as the install prefix
+# This will install tlfloat to /usr/local during the build process
+cmake -DSLEEF_BUILD_QUAD=ON -S . -B build -DCMAKE_INSTALL_PREFIX=/usr/local
+
+# Build with sudo so ext_tlfloat can install to /usr/local during build
+sudo cmake --build build -j --clean-first
+
+# Install sleef libraries to /usr/local
+sudo cmake --install build
+
+# Uncomment to uninstall:
+# sudo xargs rm -v < build/install_manifest.txt
+
+# rm -rf build
+
+# cmake \
+#   -DCMAKE_C_COMPILER=clang \
+#   -DCMAKE_CXX_COMPILER=clang++ \
+#   -DCMAKE_C_FLAGS="-fsanitize=thread -g -O1" \
+#   -DCMAKE_CXX_FLAGS="-fsanitize=thread -g -O1" \
+#   -DCMAKE_EXE_LINKER_FLAGS="-fsanitize=thread" \
+#   -DCMAKE_SHARED_LINKER_FLAGS="-fsanitize=thread" \
+#   -DSLEEF_BUILD_QUAD=ON \
+#   -DSLEEF_BUILD_TESTS=OFF \
+#   -S . -B build
+
+# cmake --build build -j
+
+# sudo cmake --install build --prefix=/usr/local
